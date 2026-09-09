03e656d0b322d3c772636067004121e4135e36e7
riddhipartyshop <ashishvisionn@gmail.com>
2026-09-09 17:39:32 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,57 @@
-const CACHE='rps-v2';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))));
+const CACHE = 'rps-v4';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+
+    fetch(event.request)
+      .then(response => {
+
+        const copy = response.clone();
+
+        caches.open(CACHE)
+          .then(cache =>
+            cache.put(event.request, copy)
+          );
+
+        return response;
+
+      })
+      .catch(() =>
+        caches.match(event.request)
+          .then(response =>
+            response || caches.match('./index.html')
+          )
+      )
+
+  );
+
+});
