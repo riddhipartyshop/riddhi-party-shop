dda5bd68b67552cb82f73a5f7e20af5603fab5c5
riddhipartyshop <ashishvisionn@gmail.com>
2026-09-09 19:39:08 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,57 +1,55 @@
-const CACHE = 'rps-v4';
+const CACHE="rps-v6";
 
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
+const ASSETS=[
+"./",
+"./index.html",
+"./sw.js",
+"./manifest.json"
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
+self.addEventListener("install",event=>{
+event.waitUntil(
+caches.open(CACHE)
+.then(cache=>cache.addAll(ASSETS))
+.then(()=>self.skipWaiting())
+);
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE)
-          .map(key => caches.delete(key))
-      )
-    ).then(() => self.clients.claim())
-  );
+self.addEventListener("activate",event=>{
+event.waitUntil(
+caches.keys()
+.then(keys=>
+Promise.all(
+keys
+.filter(key=>key!==CACHE)
+.map(key=>caches.delete(key))
+)
+)
+.then(()=>self.clients.claim())
+);
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch",event=>{
 
-  if (event.request.method !== 'GET') return;
+event.respondWith(
 
-  event.respondWith(
+fetch(event.request)
+.then(response=>{
 
-    fetch(event.request)
-      .then(response => {
+const copy=response.clone();
 
-        const copy = response.clone();
+caches.open(CACHE)
+.then(cache=>{
+cache.put(event.request,copy);
+});
 
-        caches.open(CACHE)
-          .then(cache =>
-            cache.put(event.request, copy)
-          );
+return response;
 
-        return response;
+})
+.catch(()=>
+caches.match(event.request)
+)
 
-      })
-      .catch(() =>
-        caches.match(event.request)
-          .then(response =>
-            response || caches.match('./index.html')
-          )
-      )
-
-  );
+);
 
 });
