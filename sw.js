de1ba359ec0318e8bda0621c105ad97a00edfb75
riddhipartyshop <ashishvisionn@gmail.com>
2026-09-09 16:40:24 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-const CACHE='rps-v2';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))));
